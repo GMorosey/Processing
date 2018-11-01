74f7387c0c7ee7821f88edcb94f0d43057a23da1
George Moore <43809006+George-M13@users.noreply.github.com>
2018-11-01 00:52:33 +0000

    Add files via upload

---
+++ GMGamesEngineProgramming/IntelliJ/GMGamesEngineProgramming-master/EclipseWorkingDirectory/EclipseBasic/src/game_engine2D/GameObject.java
@@ -0,0 +1,41 @@
+package game_engine2D;
+import java.util.ArrayList;
+
+import processing.core.PApplet;
+import processing.core.PVector;
+
+public abstract class GameObject extends ProcessingEntity {
+	public ArrayList<GameComponent> components;
+	public BoxCollider2D boxCol;
+	public BoundingBox bb;
+	public boolean collided;
+	public GameObject(PApplet p) {
+		super(p);
+		this.components = new ArrayList<GameComponent>();
+	}
+
+	public String name;
+	public String tag;
+	public Transform transform = new Transform();
+	public abstract void start();
+	public abstract void update();
+	public abstract void render();
+	public String ToString() {
+		return this.name;
+	}
+	public boolean colCheck(GameObject g, ArrayList<BoundingBox> bbs) {
+		BoundingBox gBB = g.transform._BoundingBox();
+		for(BoundingBox bbA : bbs) {
+
+			if(gBB.left < bbA.right && gBB.right > bbA.left) {
+				if(gBB.bottom > bbA.top  /*gBB.top < bbA.bottom*/){
+					g.transform.position.y = bbA.top;
+					System.out.println(g.transform.position.y);
+					System.out.println(bbA.top);
+					return true;
+					}
+}
+		}
+		return false;
+	}
+}
---
+++ GMGamesEngineProgramming/IntelliJ/GMGamesEngineProgramming-master/README.md
@@ -0,0 +1,1 @@
+# GMGamesEngineProgramming-master
